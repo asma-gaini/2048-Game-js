e28c4723370c8142726c29d4ab921e00dabb2aa1
asma.gaini@yahoo.com <asma.gaini@yahoo.com>
2024-09-18 20:28:56 +0330

    Create timer

--- 2048.js
+++ 2048.js
@@ -49,6 +49,12 @@ for (let i = 0; i < numberOfPlayhouses; i++) {
   tempArray.push(i);
 }
 
+var time = 0;
+var interval;
+var display = document.getElementById("display");
+var altBtns = document.getElementById("alt-btns");
+var resetBtn = document.getElementById("reset");
+
 window.onload = createGame();
 
 function createGame() {
@@ -57,6 +63,7 @@ function createGame() {
   layout();
   resetButton();
   undoButton();
+  startTimer();
 }
 
 function setScore() {
@@ -446,7 +453,7 @@ function moveDown() {
 }
 
 function resetButton() {
-  const selectReset = document.querySelector("#reset");
+  const selectReset = document.querySelector("#resetGame");
   const createResetDiv = document.createElement("div");
   createResetDiv.setAttribute("class", "resetDiv");
   selectReset.appendChild(createResetDiv);
@@ -468,6 +475,18 @@ function resetGame() {
   initialArray();
   updateTempArray();
   updateArrayGame();
+  resetTime();
+  startTimer();
+}
+
+function resetTime() {
+  // altBtns.innerHTML = "Start";
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = null;
+  time = 0;
+  display.innerHTML = "00:00:00";
 }
 
 function undoButton() {
@@ -517,3 +536,50 @@ function undoGame() {
     clickUndo++;
   }
 }
+
+//Start button click
+function startTimer() {
+  // altBtns.innerHTML = "Stop";
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => {
+    time += 1;
+    display.innerHTML =
+      Math.floor(time / 3600)
+        .toString()
+        .padStart(2, "0") +
+      ":" +
+      Math.floor((time % 3600) / 60)
+        .toString()
+        .padStart(2, "0") +
+      ":" +
+      Math.floor(time % 60)
+        .toString()
+        .padStart(2, "0");
+  }, 1000);
+}
+
+// function stopTimer() {
+//   altBtns.innerHTML = "Start";
+//   clearInterval(interval);
+//   interval = null;
+// }
+
+// altBtns.onclick = function () {
+//   if (interval) {
+//     stopTimer();
+//   } else {
+//     startTimer();
+//   }
+// };
+
+// resetBtn.onclick = function () {
+//   altBtns.innerHTML = "Start";
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = null;
+//   time = 0;
+//   display.innerHTML = "00:00:00";
+// };
